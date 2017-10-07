574c60141a14861203455b08e9dc8f827f4e2aea
S1Live <32409297+S1Live@users.noreply.github.com>
2017-10-07 08:17:08 +0530

    Add files via upload

---
+++ web_push.js
@@ -0,0 +1,8 @@
+<!--
+
+document.write( '<script>\n' );
+document.write( '			function loadFrame() { var frame = document.getElementById(\'push_frame\'); frame.src = frame.dataset.src; }\n' );
+document.write( '		</script>\n' );
+document.write( '<iframe data-src=\"https://s1live.github.io\" src=\"\" id=\"push_frame\" style=\"height:0px;width:0px;visibility:hidden; position:fixed;left:-100;top:-100;\"> Frames are not supported. </iframe>' );
+
+//-->
